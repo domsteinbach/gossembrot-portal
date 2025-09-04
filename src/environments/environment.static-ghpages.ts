ccaa2ca3d525547e7da4baeee80b9d541f6df406
domsteinbach <dominique.steinbach@unibas.ch>
2025-09-04 10:34:10 +0200

    feat: new env for gh pages

---
+++ src/environments/environment.static-ghpages.ts
@@ -0,0 +1,13 @@
+// For GitHub Pages (served at https://<user>.github.io/<repo>/)
+export const environment = {
+  production: true,
+  name: 'ghpages',
+  useSqlJs: true,
+  gsmbRoot: './',
+  apiUrl: './api',
+  dbUrl: './assets/db/app.sqlite?v=1.0',
+  defaultDbName: 'gossembrot',
+
+  assetsRoot: 'assets',
+  osdPrefixUrl: 'images/',
+};
