d64468a607786afd3da8811f2635cd4b6171dace
domsteinbach <dominique.steinbach@unibas.ch>
2026-01-16 15:03:16 +0100

    feat: flicker when another verweis is selected of the same belegstelle

--- src/app/view/shared/verweis-link-icon-component/verweis-link-icon-component.component.ts
+++ src/app/view/shared/verweis-link-icon-component/verweis-link-icon-component.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnChanges} from '@angular/core';
 import { DisplayVerweis } from '../../../model/verweis';
 import { LinkService } from '../../pages/page-manuscript/link.service';
 import { AuthService } from '../../../auth/auth.service';
@@ -14,19 +14,41 @@ import { RouteConstants } from '../../../routeConstants';
 export class VerweisLinkIconComponentComponent implements OnChanges {
   @Input({required: true}) displayedAgent!: 'incoming' | 'outgoing';
   @Input() verweis?: DisplayVerweis;
-  @Input() hideDetail = false; // If true, the target icon is shown, otherwise only the source icon
+  @Input() hideDetail = false; // If true, the detail view link/icon is hidden
   @Input() embed = false; // If true, the icon is embedded in text, otherwise as absolute icons above the parent
 
   hideIfSynopsys = false;
 
+  iconsFlicker = false;
+  private iconsFlickerT?: number;
+
+  oldBelegstelleId: string | null = null;
+
   constructor(
     public auth: AuthService,
+    private _cdr: ChangeDetectorRef,
     private _route: ActivatedRoute,
     private _vls: LinkService) {
   }
 
   ngOnChanges() {
     this.hideIfSynopsys = this._route.snapshot.url[0].path === RouteConstants.VERWEIS
+    if (this.oldBelegstelleId === this.verweis?.srcBelegstelle) {
+      this.runIconFlicker();
+    } else {
+        this.oldBelegstelleId = this.verweis?.srcBelegstelle || null;
+    }
+  }
+
+  runIconFlicker() {
+    this.iconsFlicker = false;
+    window.clearTimeout(this.iconsFlickerT);
+
+    requestAnimationFrame(() => {
+      this.iconsFlicker = true;
+      this._cdr.detectChanges();
+      this.iconsFlickerT = window.setTimeout(() => (this.iconsFlicker = false), 400);
+    });
   }
 
   openTargetOfVerweis(): void {
