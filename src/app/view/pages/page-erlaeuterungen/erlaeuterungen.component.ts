6a72e134459cffac295141c247e65c076e256257
domsteinbach <dominique.steinbach@unibas.ch>
2026-01-29 10:16:17 +0100

    refactor: change markup of unl, adjust erlaeuterungen accordingly

--- src/app/view/pages/page-erlaeuterungen/erlaeuterungen.component.ts
+++ src/app/view/pages/page-erlaeuterungen/erlaeuterungen.component.ts
@@ -11,11 +11,14 @@ import { Component } from '@angular/core';
 export class ErlaeuterungenComponent {
   readonly addExample = '<add>...</add>';
   readonly underlineExample = '<underline>unterstrichen</underline>';
-  readonly gbExample = '<k><w>grau</w><gb><w>grün</w></gb></k>';
+  readonly gbExample = '<k><w>hier wird die unkorrigierte Version angezeigt.</w><gb><w>in grüner Farbe</w></gb></k>';
+  readonly ngbExample = '<w><handshift new="#notgb" customTag="ngb"/><add hand="not gossembrot" customTag="ngb"><w>in grauer Farbe</w></add></w>';
   readonly delExmaple = '<del>durchgestrichen</del>';
   readonly undelExample = '<del undo="true">unterpunktet</del>';
   readonly teiFragExampleHrsg = '<supplied>Kursiv</supplied>';
-  readonly teiFragExample = '<frag>:::</frag>';
+  readonly teiFragExample = '<frag>:</frag>';
+  readonly teiFragExampleb = '<frag>::</frag>';
+  readonly teiFragExamplec = '<frag>:::</frag>';
   readonly teiSicExample = '<sic></sic>';
   readonly teiSymbolExample =
     '<symbol type="Kreis" desc="Kreis"></symbol>';
