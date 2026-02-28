e1477741e89a58d1d651e50fbfd45a653e655935
domsteinbach <dominique.steinbach@unibas.ch>
2026-02-28 15:03:29 +0100

    bump version

--- src/environments/environment.static-ghpages.ts
+++ src/environments/environment.static-ghpages.ts
@@ -5,7 +5,7 @@ export const environment = {
   useSqlJs: true,
   gsmbRoot: "/gossembrot-portal/",
   apiUrl: "./api",
-  dbVersion: "2.4",
+  dbVersion: "2.5",
   defaultDbName: "gossembrot",
   assetsRoot: "assets",
   osdPrefixUrl: "images/",
